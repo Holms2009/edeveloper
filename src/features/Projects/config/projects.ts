7a9ae2c63ec1ec774f7ba309f914427a17c6d0c1
Holms2009 <holms200990@gmail.com>
2023-06-30 23:44:55 +0300

    Some styles fixes

--- src/features/Projects/config/projects.ts
+++ src/features/Projects/config/projects.ts
@@ -1,11 +1,12 @@
 import promo from '../assets/promo.png';
 import avito from '../assets/avito.png';
+import codeservice from '../assets/codeservice.png';
 
 export const projects: TProject[] = [
   {
     name: 'Evgen Developer',
     link: '/',
-    stack: ['React', 'TypeScript', 'TailwindCSS', 'Vite', 'framer-motion'],
+    stack: ['React', 'TypeScript', 'TailwindCSS', 'CSS-Modules', 'Vite', 'framer-motion'],
     description: 'Моя личная страница. Собственно, тут вы и находитесь.',
     img: promo
   },
@@ -15,5 +16,12 @@ export const projects: TProject[] = [
     stack: ['JavaScript', 'PUG', 'SCSS', 'GSAP'],
     description: '',
     img: avito
+  },
+  {
+    name: 'Code + Service',
+    link: 'https://codeservice.pro',
+    stack: ['React', 'Redux', 'redux-toolkit', 'React-router', 'TypeScript', 'SCSS', 'Webpack', '@tanstack/react-table'],
+    description: '',
+    img: codeservice
   }
 ]
