5dcd369399c52b0f03f56a0ee2a223a2222c71a9
Holms2009 <holms200990@gmail.com>
2023-11-28 12:04:21 +0300

    Added kitchen project

--- src/features/Projects/config/projects.ts
+++ src/features/Projects/config/projects.ts
@@ -1,15 +1,9 @@
 import promo from '../assets/promo.png';
 import avito from '../assets/avito.png';
 import codeservice from '../assets/codeservice.png';
+import kitchen from '../assets/kitchen.png';
 
 export const projects: TProject[] = [
-  {
-    name: 'Evgen Developer',
-    link: '/',
-    stack: ['React', 'TypeScript', 'TailwindCSS', 'CSS-Modules', 'Vite', 'framer-motion'],
-    description: 'Моя личная страница. Собственно, тут вы и находитесь.',
-    img: promo
-  },
   {
     name: 'Цифровой музей архитектуры России',
     link: 'https://museum.avito.ru/',
@@ -23,5 +17,12 @@ export const projects: TProject[] = [
     stack: ['React', 'Redux', 'redux-toolkit', 'React-router', 'TypeScript', 'SCSS', 'Webpack', '@tanstack/react-table'],
     description: 'Сервис подбора и найма разработчиков, дизайнеров и т.д. Проект находится в активной разработке.',
     img: codeservice
+  },
+  {
+    name: 'Kitchen Consulting',
+    link: 'https://kitchen.consulting',
+    stack: ['JavaScript', 'CSS', 'Webflow'],
+    description: 'Лэндинг для Kitchen Consulting. По желанию клиента, сайт построен на базе конструктора Webflow. Анимация переходов между блоками - видео, управляемое из кастомного JS-кода.',
+    img: kitchen
   }
 ]
