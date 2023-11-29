24d5d3e61c24f4ca3499b6e8f401cfec3140465c
Holms2009 <holms200990@gmail.com>
2023-11-29 16:59:50 +0300

    Added yandex-letual

--- src/features/Projects/config/projects.ts
+++ src/features/Projects/config/projects.ts
@@ -1,13 +1,13 @@
-import promo from '../assets/promo.png';
 import avito from '../assets/avito.png';
 import codeservice from '../assets/codeservice.png';
 import kitchen from '../assets/kitchen.png';
+import letual from '../assets/letual.png';
 
 export const projects: TProject[] = [
   {
     name: 'Цифровой музей архитектуры России',
     link: 'https://museum.avito.ru/',
-    stack: ['JavaScript', 'PUG', 'SCSS', 'GSAP'],
+    stack: ['JavaScript', 'PUG', 'SCSS', 'GSAP', 'Webpack'],
     description: 'Промо-сайт для авито. Основной упор сделан на анимации. Проект завершен.',
     img: avito
   },
@@ -24,5 +24,12 @@ export const projects: TProject[] = [
     stack: ['JavaScript', 'CSS', 'Webflow'],
     description: 'Лэндинг для Kitchen Consulting. По желанию клиента, сайт построен на базе конструктора Webflow. Анимация переходов между блоками - видео, управляемое из кастомного JS-кода.',
     img: kitchen
+  },
+  {
+    name: 'Яндекс х Лэтуаль',
+    link: 'https://travel.yandex.ru/special/aromatour-letu',
+    stack: ['TypeScript', 'SCSS', 'PUG', 'anime.js', 'Parcel'],
+    description: 'Лэндинг для коллаборации Яндекс.Путешествия и Лэтуаль. Наиболее интересный момент - блоки с цветами. Анимированы с помощью библиотеки anime.js. Анимация работает при появлении блоков на экране + анимация при движении мышью.',
+    img: letual
   }
 ]
